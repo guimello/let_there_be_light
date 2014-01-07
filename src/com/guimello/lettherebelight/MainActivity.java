22981ebc82a799e6c0cac5e624bb8aa2b530c526
guimello <guilhermesilvamello@gmail.com>
2014-01-07 15:09:42 -0200

    Destroying the camera on activity being dismissed

--- src/com/guimello/lettherebelight/MainActivity.java
+++ src/com/guimello/lettherebelight/MainActivity.java
@@ -11,10 +11,11 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
 	private static final String TAG = MainActivity.class.getSimpleName();
 	private static final String LIGHT_ON_TAG = "LIGHT_ON";
 	
-	private static Camera camera;
+	private Camera camera;
 	private boolean lightOn;
 
 	@Override
@@ -22,24 +23,22 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		getCamera();
+		startCamera();
 	}
 
 	@Override
 	public void onSaveInstanceState(Bundle savedInstanceState) {
 		super.onSaveInstanceState(savedInstanceState);
-
 		savedInstanceState.putBoolean(LIGHT_ON_TAG, lightOn);
 	}
 
 	@Override
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-
 		lightOn = savedInstanceState.getBoolean(LIGHT_ON_TAG);
 	}
 
-	private void getCamera() {
+	private void startCamera() {
 		if (camera == null) {
 			try {
 				camera = Camera.open();
@@ -64,52 +63,40 @@ public class MainActivity extends Activity {
 	private void turnLightOn() {
 		if (camera == null) {
 	    	Toast.makeText(this, "Camera not found", Toast.LENGTH_LONG).show();
-	    	// TODO: Use the screen as a flashlight (next best thing)
 	    	return;
 	    }
 	    
 	    lightOn = true;
 	    
 	    Parameters parameters = camera.getParameters();
-	    if (parameters == null) {
-	    	// TODO: Use the screen as a flashlight (next best thing)
-	    	return;
-	    }
+	    if (parameters == null) return;
 	    
 	    List<String> flashModes = parameters.getSupportedFlashModes();
 	    // Check if camera flash exists
-	    if (flashModes == null) {
-	    	// TODO: Use the screen as a flashlight (next best thing)
-	    	return;
-	    }
-	    
+	    if (flashModes == null) return;
+
 	    String flashMode = parameters.getFlashMode();
+
 	    Log.i(TAG, "Flash mode: " + flashMode);
 	    Log.i(TAG, "Flash modes: " + flashModes);
-	    
-	    // Maybe it is on already
-	    // TODO: Remove this check
-	    if (!Parameters.FLASH_MODE_TORCH.equals(flashMode)) {
-	    	// Turn on the flash
-	    	if (flashModes.contains(Parameters.FLASH_MODE_TORCH)) {
-	    		parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
-	    		camera.setParameters(parameters);
-	    		// TODO: Forbid sleeping
-	    	} else {
-	    		Toast.makeText(
-	    			this,
-	    			"Flash mode (torch) not supported",
-	    			Toast.LENGTH_LONG
-	    		).show();
-	        
-	    		// TODO: Use the screen as a flashlight (next best thing)
-	    		Log.e(TAG, "FLASH_MODE_TORCH not supported");
-	    	}
+
+	    // Turn on the flash
+	    if (flashModes.contains(Parameters.FLASH_MODE_TORCH)) {
+	    	parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+	    	camera.setParameters(parameters);
+	    } else {
+	    	Toast.makeText(
+	    		this,
+	    		"Flash mode (torch) not supported",
+	    		Toast.LENGTH_LONG
+	    	).show();
+
+	    	Log.e(TAG, "FLASH_MODE_TORCH not supported");
 	    }
 	}
-	
+
 	private void turnLightOff() {
-		if (lightOn || true) {
+		if (lightOn) {
 			lightOn = false;
 	      
 			if (camera == null) return;
@@ -118,24 +105,40 @@ public class MainActivity extends Activity {
 			if (parameters == null) return;
 	      
 			List<String> flashModes = parameters.getSupportedFlashModes();
-			String flashMode = parameters.getFlashMode();
-			
 			// Check if camera flash exists
 			if (flashModes == null) return;
+			
+			String flashMode = parameters.getFlashMode();
 
 			Log.i(TAG, "Flash mode: " + flashMode);
 			Log.i(TAG, "Flash modes: " + flashModes);
+			
+			// Turn on the flash briefly before turning off
+			// This avoids an issues when the flash is on but the flash
+			// mode returned is off
+		    if (flashModes.contains(Parameters.FLASH_MODE_TORCH)) {
+		    	parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		    	camera.setParameters(parameters);
+		    }
 
-			if (!Parameters.FLASH_MODE_OFF.equals(flashMode)) {
-				// Turn off the flash
-				if (flashModes.contains(Parameters.FLASH_MODE_OFF)) {
-					parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
-					camera.setParameters(parameters);
-					// TODO: Remove sleep prevention
-				} else {
-					Log.e(TAG, "FLASH_MODE_OFF not supported");
-				}
+			// Turn off the flash
+			if (flashModes.contains(Parameters.FLASH_MODE_OFF)) {
+				parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+				camera.setParameters(parameters);
+			} else {
+				Toast.makeText(this, "Could not turn off the flash", Toast.LENGTH_LONG).show();
+				Log.e(TAG, "FLASH_MODE_OFF not supported");
 			}
 	    }
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		if (camera != null) {
+			camera.stopPreview();
+			camera.release();
+		}
 	}
 }
